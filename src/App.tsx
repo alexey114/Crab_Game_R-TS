67201e38a099cc4207ce721540a2899ae25655d0
alexey114 <bomko.alexey@cartweb.ru>
2022-02-21 18:23:21 +0300

    add start, rules ant time screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [rulesGame, setRulesGame] = useState(
+    <div className="start__page">
+      <h1>Crab game</h1>
+      <a onClick={showRulesGame}>START</a>
+    </div>
+  )
+
+  function showRulesGame() {
+    setRulesGame(
+      <div className="rules__game">
+        <h1>ПРАВИЛА ИГРЫ</h1>
+        <p>Наводи курсором на крабов <img src="../img/crab.png" alt="crab" /> и нажимай левую кнопку мышки, тем самым лови их. Чем больше поймаешь, тем сытней тебе будет жить на острове! Удачи мой друг и обрати внимание на пальмы, они часто там прячутся</p>
+        <button className='buttonYes' onClick={startGame}>Понятно!</button>
+      </div>
+    )
+  }
+
+  function startGame() {
+    setRulesGame(
+      <div className="settings__game">
+      <h1>Время:</h1>
+      <ul className="time__game" id="time__game">
+          <li><button className="btn__game" data-time="15">15 сек</button></li>
+          <li><button className="btn__game" data-time="30">30 сек</button></li>
+          <li><button className="btn__game" data-time="45">45 сек</button></li>
+          <li><button className="btn__game" data-time="60">60 сек</button></li>
+      </ul>
+  </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {rulesGame}
     </div>
   );
 }

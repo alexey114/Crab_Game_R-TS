0c982df533821849db87d8c90cd7e9b23c298a88
BomkoAR <bomko.alexey@cartweb.ru>
2022-02-27 00:37:43 +0300

    add finish + return game

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { setInterval } from 'timers';
 import './App.css';
 
 function App() {
@@ -10,13 +11,17 @@ function App() {
     </div>
   )
 
-  let [timer, setTimer] = useState(15)
+  let [timer, setTimer] = useState<number>(10)
+  let [coordinateCrab, setCoordinateCrab] = useState([105, 105])
+  let [fifteenSeconds, setFifteenSeconds] = useState(false);
+  let [thirtySeconds, setThirtySeconds] = useState(false);
+  // let [fifteenSeconds, setFifteenSeconds] = useState(false);
 
   function showRulesGame() {
     setRulesGame(
       <div className="rules__game">
         <h1>ПРАВИЛА ИГРЫ</h1>
-        <p>Наводи курсором на крабов <img src="/img/crab.png" alt="crab" /> и нажимай левую кнопку мышки, тем самым лови их. Чем больше поймаешь, тем сытней тебе будет жить на острове! Удачи мой друг и обрати внимание на пальмы, они часто там прячутся</p>
+        <p>Наводи курсором на крабов <img src="https://papik.pro/uploads/posts/2021-09/thumbs/1630675178_1-papik-pro-p-krabik-risunok-detskii-1.png" alt="crab" style={{ width: 45, background: "none" }} /> и нажимай левую кнопку мышки, тем самым лови их. Чем больше поймаешь, тем сытней тебе будет жить на острове! Удачи мой друг и обрати внимание на пальмы, они часто там прячутся</p>
         <button className='buttonYes' onClick={startGame}>Понятно!</button>
       </div>
     )
@@ -27,33 +32,81 @@ function App() {
       <div className="settings__game">
         <h1>Время:</h1>
         <ul className="time__game" id="time__game">
-          <li><button className="btn__game" data-time="15" onClick={game}>15 сек</button></li>
-          <li><button className="btn__game" data-time="30" onClick={game}>30 сек</button></li>
-          <li><button className="btn__game" data-time="45" onClick={game}>45 сек</button></li>
-          <li><button className="btn__game" data-time="60" onClick={game}>60 сек</button></li>
+          <li><button className="btn__game" onClick={game}>15 сек</button></li>
+          <li><button className="btn__game" onClick={game30s}>30 сек</button></li>
+          <li><button className="btn__game" onClick={game45s}>45 сек</button></li>
+          <li><button className="btn__game" onClick={game60s}>60 сек</button></li>
         </ul>
       </div>
     )
-    setTimer(parseInt(event.target.getAttribute('data-time')))
+  }
+
+  function timeLeft() {
+    timer--
+    console.log("time", timer)
+    return <h3>Осталось {timer} сек</h3>
+  }
+
+  function game30s() {
+    let time = timer + 15
+    setTimer(time)
+    console.log("time30", time)
+    console.log("timer30", timer)
+    setInterval(timeLeft, 1000)
+    game()
+  }
+
+  console.log("вне timer30", timer)
+
+  function game45s() {
+    setTimer(45)
+
+  }
+
+  function game60s() {
+    setTimer(60)
+    game()
   }
 
   function game() {
 
     setRulesGame(
       <div className="field__game">
-        <h3>Осталось {timer} сек</h3>
+        {() => timeLeft()}
         <div className="board__game" id="board__game">
-          <img className="board__game_img" src="/img/img_crab_game.jpg" alt="img_crab_game"/>
+          <img className="board__game_img" src="https://i.7fon.org/1000/c156069.jpg" alt="img_crab_game" />
+          <img className="crab__game_img" onMouseDown={downCrab} src="https://papik.pro/uploads/posts/2021-09/thumbs/1630675178_1-papik-pro-p-krabik-risunok-detskii-1.png" alt="crab" style={{ width: 45, top: coordinateCrab[0], left: coordinateCrab[1] }} />
         </div>
       </div>
     )
   }
 
-  function finishGame() {
-    <div>
-      <div className="hide" id="primary__page"><a href="/#">Хочу еще крабов!</a></div>
-      <div className="hide" id="copy__text"><a href="https://wed-developer-bomkoar.000webhostapp.com" className="copy">&#169 Bomko Aleksey</a></div>
-    </div>
+  function downCrab() {
+    let coordinate = Math.random() * (500 - 0) + 0
+    setCoordinateCrab([coordinate, coordinate])
+    console.log("downCrab")
+    if (timer > 0) {
+      setRulesGame(
+        <div className="field__game">
+          <h3>Осталось {timer} сек</h3>
+          <div className="board__game" id="board__game">
+            <img className="board__game_img" src="https://i.7fon.org/1000/c156069.jpg" alt="img_crab_game" />
+            <img className="crab__game_img" onClick={downCrab} src="https://papik.pro/uploads/posts/2021-09/thumbs/1630675178_1-papik-pro-p-krabik-risunok-detskii-1.png" alt="crab" style={{ width: 45, top: coordinateCrab[0], left: coordinateCrab[1] }} />
+          </div>
+        </div>
+      )
+    } else {
+      setRulesGame(
+        <div>
+          <div className="hide" id="primary__page"><a href="/#" onClick={returnToStart}>Хочу еще крабов!</a></div>
+          <div className="hide" id="copy__text"><a href="https://wed-developer-bomkoar.000webhostapp.com" target="blank" className="copy">Bomko Aleksey</a></div>
+        </div>
+      )
+    }
+  }
+
+  function returnToStart(){
+    window.location.reload()
   }
 
   return (
